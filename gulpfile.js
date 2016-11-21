eb3ad79069487003e691123915bcbe2e514ce8f3
eteubert <eric@ericteubert.de>
2016-11-21 16:21:49 +0800

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
-var dist = require('br-wordpress-gulp-dist');
-// var dist = require('/Users/ericteubert/code/br-wordpress-gulp-dist/dist.js');
+var githubConfig = require('github-config');
+// var dist = require('br-wordpress-gulp-dist');
+var dist = require('/Users/ericteubert/code/br-wordpress-gulp-dist/dist.js');
 
 var config = {
     pluginFile: './plugin.php'
@@ -11,7 +12,8 @@ gulp.task('default', function() {
     runSequence(
         'bump-version',
         'update-plugin-file-version',
-        'make-asset'
+        'make-asset',
+        'deploy-asset'
     );
 });
 
@@ -20,4 +22,12 @@ gulp.task('update-plugin-file-version', function(cb) {
     return dist.updateWordPressPluginFile(config.pluginFile, cb);
 });
 gulp.task('make-asset', dist.makeReleaseAsset);
-gulp.task('deploy-asset', dist.deployReleaseAsset);
+gulp.task('deploy-asset', function(cb) {
+
+    return dist.deployReleaseAsset({
+        manifest: require('./package.json'),
+        token: githubConfig().token
+    });
+
+    cb();
+});
--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Example Dist Plugin
  * Plugin URI:  http://example.com
  * Description: A shining example.
- * Version:     1.0.9
+ * Version:     1.0.12
  * Author:      Eric Teubert
  * Author URI:  http://example.com
  * License:     MIT
