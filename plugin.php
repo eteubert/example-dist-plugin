a890f7603dcb7f34df7de96197ce98e550624b6d
eteubert <eric@ericteubert.de>
2016-12-07 12:47:29 +0800

    Auto-Commit for deployment 1.1.2

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Example Dist Plugin
  * Plugin URI:  http://example.com
  * Description: A shining example.
- * Version:     1.1.1
+ * Version:     1.1.2
  * Author:      Eric Teubert
  * Author URI:  http://example.com
  * License:     MIT
